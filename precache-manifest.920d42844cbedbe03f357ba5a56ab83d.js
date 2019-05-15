c3476b2e33c360adc4c6f04bab98a0e28cdcf683
ahadsenja <hamdanilm96@gmail.com>
2019-05-16 02:28:34 +0800

    Updates

---
+++ precache-manifest.920d42844cbedbe03f357ba5a56ab83d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f11446904d40781fff26cc0767b4ce",
+    "url": "/dailyactivity.reactjs/index.html"
+  },
+  {
+    "revision": "3006325fd9d315b61424",
+    "url": "/dailyactivity.reactjs/static/css/main.4a303ede.chunk.css"
+  },
+  {
+    "revision": "f49f49a68cae45f36850",
+    "url": "/dailyactivity.reactjs/static/js/2.7be4d342.chunk.js"
+  },
+  {
+    "revision": "3006325fd9d315b61424",
+    "url": "/dailyactivity.reactjs/static/js/main.4853f282.chunk.js"
+  },
+  {
+    "revision": "6a1019077ea4ef3fd7e0",
+    "url": "/dailyactivity.reactjs/static/js/runtime~main.21b387ac.js"
+  }
+]);
